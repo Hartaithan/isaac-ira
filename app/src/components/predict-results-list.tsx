6a765f70ee48fccceeb5007b561d23f124163919
Hartaithan <hartaithan@gmail.com>
2024-12-20 11:00:14 +0900

    add wrapper to center item images

--- app/src/components/predict-results-list.tsx
+++ app/src/components/predict-results-list.tsx
@@ -1,4 +1,5 @@
 import { items } from "@/constants/items";
+import { Item } from "@/model/item";
 import { PredictResult } from "@/model/predict";
 import { usePrediction } from "@/providers/prediction";
 import { getItemStyles } from "@/utils/item";
@@ -8,12 +9,36 @@ interface ItemProps {
   result: PredictResult;
 }
 
+interface ItemImageProps {
+  size?: number;
+  item: Item;
+}
+
+const ItemImage: FC<ItemImageProps> = (props) => {
+  const { size = 50, item } = props;
+  return (
+    <div className="overflow-hidden" style={{ width: size, height: size }}>
+      <div
+        className="relative scale-50"
+        style={{ width: size * 2, height: size * 2 }}
+      >
+        <img
+          className="absolute -translate-x-1/2 -translate-y-1/2 transform"
+          src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+          style={getItemStyles(item)}
+          alt={item.name + " " + item.description}
+        />
+      </div>
+    </div>
+  );
+};
+
 const PredictResultItem: FC<ItemProps> = memo((props) => {
   const { result } = props;
   const item = items[result.id];
   return (
-    <div className="flex min-w-24 flex-col items-center justify-center rounded bg-neutral-900/80 px-3 py-2 text-center text-white">
-      <img style={getItemStyles(item)} />
+    <div className="flex min-w-24 flex-col items-center rounded bg-neutral-900/80 px-3 py-2 text-center text-white">
+      <ItemImage item={item} />
       <p className="font-bold">{item?.name || "Not found"}</p>
       <p>{result.probability + "%"}</p>
     </div>
--- app/src/utils/item.ts
+++ app/src/utils/item.ts
@@ -3,10 +3,11 @@ import { CSSProperties } from "react";
 
 export const getItemStyles = (item: Item): CSSProperties => {
   const { image_url, position, width, height } = item;
+  const [x, y] = position;
   return {
     backgroundImage: `url(./images/${image_url.replace(".png", ".avif")})`,
-    backgroundPositionX: `-${position[0]}px`,
-    backgroundPositionY: `${position[1]}px`,
+    backgroundPositionX: `-${x}px`,
+    backgroundPositionY: `${y}px`,
     backgroundRepeat: "no-repeat",
     width,
     height,
